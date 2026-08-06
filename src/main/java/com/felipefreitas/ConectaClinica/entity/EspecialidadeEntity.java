48cf8f05913d37e5b34cc8e6071f2f6e0933e564
felipe.freitas <felipe.freitas@foursys.com.br>
2026-08-06 14:23:59 -0300

    Atualizando EspecialidadeEntity

--- src/main/java/com/felipefreitas/ConectaClinica/entity/EspecialidadeEntity.java
+++ src/main/java/com/felipefreitas/ConectaClinica/entity/EspecialidadeEntity.java
@@ -1,10 +1,7 @@
 package com.felipefreitas.ConectaClinica.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "especialidades")
@@ -12,6 +9,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class EspecialidadeEntity {
 
     @Id
