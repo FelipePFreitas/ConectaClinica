7dd2f2f13c9d0393f6de7748df50f59f84636505
felipe.freitas <felipe.freitas@foursys.com.br>
2026-08-06 08:37:29 -0300

    Atualizando o CargoResponseDTO

--- src/main/java/com/felipefreitas/ConectaClinica/dto/cargo/CargoResponseDTO.java
+++ src/main/java/com/felipefreitas/ConectaClinica/dto/cargo/CargoResponseDTO.java
@@ -1,4 +1,16 @@
 package com.felipefreitas.ConectaClinica.dto.cargo;
 
-public record CargoResponseDTO(Long id, String cargo) {
+import com.felipefreitas.ConectaClinica.entity.CargoEntity;
+
+public record CargoResponseDTO(
+        Long id,
+        String cargo
+) {
+
+    public CargoResponseDTO(CargoEntity cargoEntity) {
+            this(
+                    cargoEntity.getId(),
+                    cargoEntity.getNome()
+            );
+        }
 }
