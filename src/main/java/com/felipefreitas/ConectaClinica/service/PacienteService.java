f266abb1eaef247005ee5412b18930634ad0381c
felipe.freitas <felipe.freitas@foursys.com.br>
2026-08-12 10:18:46 -0300

    Adicionando Mapper de Paciente

--- src/main/java/com/felipefreitas/ConectaClinica/service/PacienteService.java
+++ src/main/java/com/felipefreitas/ConectaClinica/service/PacienteService.java
@@ -5,6 +5,7 @@ import com.felipefreitas.ConectaClinica.dto.paciente.PacienteResponseDTO;
 import com.felipefreitas.ConectaClinica.entity.PacienteEntity;
 import com.felipefreitas.ConectaClinica.enums.ErrorEnum;
 import com.felipefreitas.ConectaClinica.exceptions.BaseException;
+import com.felipefreitas.ConectaClinica.mapper.PacienteMapper;
 import com.felipefreitas.ConectaClinica.repository.PacienteRepository;
 import com.felipefreitas.ConectaClinica.util.CPFUtil;
 import lombok.RequiredArgsConstructor;
@@ -18,43 +19,33 @@ import java.time.LocalDateTime;
 public class PacienteService {
 
     private final PacienteRepository pacienteRepository;
+    private final PacienteMapper pacienteMapper;
 
 
     @Transactional
-    public PacienteResponseDTO cadastrarPaciente (PacienteRequestDTO pacienteRequestDTO){
+    public PacienteResponseDTO cadastrarPaciente(PacienteRequestDTO pacienteRequestDTO) {
 
         String cpfLimpo = CPFUtil.cleanCpf(pacienteRequestDTO.cpf());
 
-        if (!CPFUtil.isValid(cpfLimpo)){
+        if (!CPFUtil.isValid(cpfLimpo)) {
             throw new BaseException(ErrorEnum.CPF_INVALIDO);
         }
 
-        if (pacienteRepository.existsByCpf(cpfLimpo)){
+        if (pacienteRepository.existsByCpf(cpfLimpo)) {
             throw new BaseException(ErrorEnum.CPF_PACIENTE_JA_CADASTRADO);
         }
 
         String emailNormalizado = pacienteRequestDTO.email().toLowerCase().trim();
 
-        if (pacienteRepository.existsByEmail(emailNormalizado)){
+        if (pacienteRepository.existsByEmail(emailNormalizado)) {
             throw new BaseException(ErrorEnum.EMAIL_PACIENTE_JA_CADASTRADO);
         }
 
-        PacienteEntity paciente = PacienteEntity.builder()
-                .nome(pacienteRequestDTO.nome())
-                .cpf(cpfLimpo)
-                .email(emailNormalizado)
-                .telefone(pacienteRequestDTO.telefone())
-                .dataNascimento(pacienteRequestDTO.dataNascimento())
-                .dataCadastro(LocalDateTime.now())
-                .endereco(pacienteRequestDTO.endereco())
-                .numero(pacienteRequestDTO.numero())
-                .bairro(pacienteRequestDTO.bairro())
-                .cidade(pacienteRequestDTO.cidade())
-                .estado(pacienteRequestDTO.estado())
-                .build();
+        PacienteEntity paciente = pacienteMapper.toEntity(pacienteRequestDTO);
 
         PacienteEntity pacienteSalvo = pacienteRepository.save(paciente);
 
-        return new PacienteResponseDTO(pacienteSalvo);
+        return pacienteMapper.toDTO(pacienteSalvo);
     }
+
 }
