39c02bcfc55e4ebb92254bccb689815b79380a5a
Felipe <felipefreitas21081991@gmail.com>
2026-08-15 13:14:08 -0300

    recomeçando o projeto

---
+++ src/main/java/com/felipefreitas/ConectaClinica/ConectaClinicaApplication.java
@@ -0,0 +1,13 @@
+package com.felipefreitas.ConectaClinica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaClinicaApplication.class, args);
+	}
+
+}
