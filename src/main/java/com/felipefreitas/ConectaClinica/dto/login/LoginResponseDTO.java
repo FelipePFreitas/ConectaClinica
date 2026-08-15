9c970f00e9c6922a9d375baaa6d433de7edc006a
Felipe <felipefreitas21081991@gmail.com>
2026-08-15 12:48:57 -0300

    recomeçando o projeto

--- src/main/java/com/felipefreitas/ConectaClinica/dto/login/LoginResponseDTO.java
+++
@@ -1,7 +0,0 @@
-package com.felipefreitas.ConectaClinica.dto.login;
-
-public record LoginResponseDTO(
-        String token,
-        String tipo, // Ex: "Bearer"
-        Long expiracaoEm // Ex: tempo restante ou timestamp
-) {}
--- src/main/java/com/felipefreitas/ConectaClinica/repository/TipoExameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.felipefreitas.ConectaClinica.repository;
-
-import com.felipefreitas.ConectaClinica.entity.TipoExameEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TipoExameRepository extends JpaRepository<TipoExameEntity, Long> {
-}
