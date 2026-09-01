c701b4e638535010ee8dc0abcd75399f0ba60216
felipe.freitas <felipe.freitas@foursys.com.br>
2026-09-01 15:43:56 -0300

    Remover classes e arquivos não utilizados do projeto

--- src/main/java/com/felipefreitas/ConectaClinica/repository/ConsultaRespository.java
+++
@@ -1,9 +0,0 @@
-package com.felipefreitas.ConectaClinica.repository;
-
-import com.felipefreitas.ConectaClinica.entity.ConsultaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConsultaRespository extends JpaRepository<ConsultaEntity, Long> {
-}
